2861d40ef712a9c55b05195ab026e0c71fda3908
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-11-16 15:34:13 +0530

    update

--- script.js
+++ script.js
@@ -1,27 +1,82 @@
     let score = 0;
     let wicket=0;
+    let ballwiseResult =[];
+    let hit=0;
+    let commentry=React.createRef();
 
 
-
- function addOne(){
-    // alert("Player took a Single");
-    score+=1;
-    rootElement.render(<App />);
-    console.log(score);
-    }
 
     const addScore = (num) =>{
-        score+=num;
+        
+        hit = num;
         rootElement.render(<App />);
+        
+        
     }
+
 
     function setWicket(){
-    alert("Player got Out");
-    wicket+=1;
+    
+    hit = "W";
     rootElement.render(<App />);
+  
+    
+}
+
+const ScoreButtons =()=>(
+
+     <div>
+
+        <button onClick={()=>addScore(0)}>0</button>
+        <button onClick={()=>addScore(1)}>1</button>
+        <button onClick={()=>addScore(2)}>2</button>
+        <button onClick={()=>addScore(3)}>3</button>
+        <button onClick={()=>addScore(4)}>4</button>
+        <button onClick={()=>addScore(6)}>6</button>
+        <button onClick={()=>addScore(-1)}>Deduct Run</button>
+        <button onClick={setWicket}>Wicket</button>
+    </div>
+
+)
+
+const Result = () =>(
+    <div>
+    {
+       
+        ballwiseResult.map((res,index)=>(
+         <>
+         {index % 6 ==0? <br/>: null}
+        <span key={index}>{res ==0?<strong>.</strong>:res}</span>&nbsp;&nbsp;</>))
     }
+    </div>
+)
 
+function handleSubmit(event){
+    event.preventDefault();
+    if(hit=="W"){
+        wicket+=1;
+    }else{
+    score+=hit;
+    }
+    ballwiseResult.unshift(
+        // <span>{hit}{" :- "}{commentry.current.value}</span>
+        <span>{`${hit} :- ${commentry.current.value}`}</span>
+        );
 
+    hit = 0;
+    commentry.current.value="";
+    rootElement.render(<App />);
+
+}
+
+const Form = ()=>(
+    <form onSubmit={handleSubmit}> 
+        <input value={hit}/>&nbsp;&nbsp;
+        <input ref={commentry} placeholder="Commentery"/>
+        <button>Submit</button>
+    </form>
+    
+)
 
 
 const App = () =>{
@@ -34,16 +89,16 @@ const App = () =>{
     
     <h1>SCORE KEEPER</h1>
     <h2>SCORE: {score}/{wicket}</h2>
-    <div>
-        <button onClick={()=>addScore(1)}>1</button>
-        <button onClick={()=>addScore(2)}>2</button>
-        <button onClick={()=>addScore(3)}>3</button>
-        <button onClick={()=>addScore(4)}>4</button>
-        <button onClick={()=>addScore(6)}>6</button>
-        <button onClick={()=>addScore(-1)}>Deduct Run</button>
-        <button onClick={setWicket}>Wicket</button>
+    <ScoreButtons/>
+    <br/>
+    {/* <Result /> */}
+    <Form/>
+    <hr/>
+    {ballwiseResult.map((res, index)=>(
+        <p key={index}>{res}</p>
+    ))}
     </div>
-    </div>
+
     
     )
 }
